c2b2d365c26a964543da75ffcb51b9cc0b5e30a4
DevInnovator10 <devinnovator10@gmail.com>
2021-05-26 00:00:00 -0400

    chore: update email subscription 2x

---
+++ pages/api/orders/cart.js
@@ -0,0 +1,56 @@
+import { standardError } from '../../../utils/standardizedFetch';
+import SCOPE_TYPES from '../../../utils/sentryScopeTypes';
+import API from '../../../utils/api';
+import withLogs from '../../../utils/withLogs';
+
+const handler = async (req, res) => {
+  const {
+    body,
+    cookies,
+    headers,
+    method
+  } = req;
+
+  const api = new API({
+    fingerprint: __filename.replace('pages', '').split('.')[0],
+    base: process.env.SOLIDUS_HOST,
+    host: SCOPE_TYPES.SERVICES.SOLIDUS,
+    request: req,
+    scopes: [
+      SCOPE_TYPES.API,
+      SCOPE_TYPES.SERVICES.SOLIDUS
+    ],
+    headers,
+    ...cookies
+  });
+
+  let response;
+
+  switch (method) {
+    case 'POST':
+      response = await api.setPath('/api/orders/cart').post(body);
+      res.status(response?.status ?? 500).json(response?.data ?? null);
+      break;
+
+    default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).json({
+        status: 405,
+        statusText: 'Method Not Allowed',
+        data: { errors: [standardError({ status: 405, error: `Method ${method} Not Allowed` })] }
+      });
+  }
+
+  return {
+    response,
+    method,
+    body: req?.body ?? null,
+    query: req?.query ?? null,
+    status: api?.response?.status ?? 500,
+    headers,
+    uri: '/api/orders/cart',
+    filename: __filename
+  };
+};
+
+export default withLogs(handler);
