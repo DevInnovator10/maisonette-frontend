f774ae754c56d40635682b283d57cfa90cccd763
DevInnovator10 <devinnovator10@gmail.com>
2021-10-25 00:00:00 -0400

    feat: update types

---
+++ tissues/career-content/baseCareerContentHtml.js
@@ -0,0 +1,48 @@
+const baseCareerContentHtml = `
+<section>
+  <h2>Who We Are</h2>
+  <p>Founded by two Vogue power players, and hailed by The Wall Street Journal as &ldquo;the future of kids&rsquo; style shopping,&rdquo; Maisonette is a curated online marketplace for exclusive children&rsquo;s brands and boutiques. Backed by NEA and Thrive Capital, we intend to become the global leader of a $300 billion market by approaching children&rsquo;s goods with the same respect and focus on style that has been traditionally given to women&rsquo;s and men&rsquo;s.</p>
+</section>
+
+<section>
+  <h2>What We Need</h2>
+  <p>Summary of the hiring team, and what we need from this role, with a focus on outcomes. Goals like, "increase by __%", "double by Q3 2021" are great to use. Here you should also list soft skills applicable to your team as well. See example:</p>
+  <p>Our Merchandising team is small but scrappy, so we're extremely excited to add another kid loving designer to the fold. If you consider yourself a team player with a strong work ethic, we might be the team for you. We want your creativity to shine through, while still aligning with the Maisonette brand direction and aesthetic. The perfect fit is someone who wont hesitate to go beyond the key responsibilities listed below. And, since we are still operating as a remote team, we place an even stronger emphasis on the need for clear and effective communication. If you are someone who can work in a fast paced environment, while maintaining a high quality of workmanship, we would love for you to keep reading.</p>
+</section>
+
+<section>
+  <h2>What You'll Do</h2>
+  <ul>
+    <li>Seasonal Trend research and overall children's market research - be aware of what other similar brands are doing in the market.</li>
+    <li>Lead Concept &amp; Line development - creating boards with seasonal concept, inspiration, colors, swatches, print and graphic direction.</li>
+    <li>Source fabrics for overseas counter-sourcing with factory partners.</li>
+    <li>Design for sketch reviews. Styles will be chosen according to the seasonal line plan.</li>
+    <li>Once designs are selected - work on fabric, colors, trims, embellishments for each garment. Work with product development and tech designer to ensure design is being communicated correctly and as intended to factory</li>
+    <li>Attend in-person or virtual fit sessions to assure proper fit, construction, artwork placement and trim/ embellishment application</li>
+    <li>See designs through the production process, work closely with the team to make sure the final products are inline with original designs submitted.</li>
+    <li>Collaborate cross functionally with Creative team, Marketing and Product Development on the brand identity and brand collateral</li>
+  </ul>
+
+  <p>Additional Customer Experience roles and responsibilities may include, but are not limited to, the following:</p>
+  <ul>
+    <li>Assisting with service needs such as returns, exchanges, and order issues</li>
+    <li>Troubleshooting customer issues, concerns, or complaints; de-escalating any dissatisfaction and providing solutions to improve customer experience</li>
+  </ul>
+</section>
+
+<section>
+  <h2>Qualifications</h2>
+  <ul>
+    <li>Degree in Fashion Design</li>
+    <li>Minimum of 5 years of design experience in Children's wear</li>
+    <li>Thorough knowledge of silhouettes, fabrications, sewing techniques, trims, and technical knowledge of fabric, garment construction, and patterns</li>
+  </ul>
+</section>
+
+<section>
+  <h2>HERE WE SHOULD PUT OUR DIVERSITY STATEMENT / HIRING PHILOSOPHY</h2>
+  <p>We&rsquo;re passionate about creating an inclusive workplace that promotes and values diversity. Companies that are diverse in age, gender identity, race, sexual orientation, physical or mental ability, ethnicity, and perspective are proven to be better companies. More importantly, creating an environment where everyone, from any background, can do their best work is the right thing to do. We look forward to interest in our roles from all walks of life.</p>
+</section>
+`;
+
+export default baseCareerContentHtml;
