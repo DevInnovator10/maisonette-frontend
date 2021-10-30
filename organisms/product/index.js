ea45a7d5128573e742ed45cdd831039411ade420
DevInnovator10 <devinnovator10@gmail.com>
2021-10-30 00:00:00 -0400

    feat: update popular

---
+++ organisms/product/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { updateQuickShopProduct, updateQuickShopIndex } from '../../store/modules/activeQuickshop/actions';
+
+export const Product = (Component) => (props) => (
+  <Component {...props} />
+);
+
+const mapStateToProps = (state) => ({
+  products: state.products,
+  quickShopIndex: state.activeQuickshop.index,
+  quickShopProductId: state.activeQuickshop.id
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  updateQuickShopIndex: (index) => dispatch(updateQuickShopIndex(index)),
+  updateQuickShopProduct: (id) => dispatch(updateQuickShopProduct(id))
+});
+
+export default compose(connect(mapStateToProps, mapDispatchToProps), Product);
