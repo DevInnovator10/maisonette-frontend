3eba4915b8c5faa2d7972d604780455ba1cc7777
DevInnovator10 <devinnovator10@gmail.com>
2021-12-25 00:00:00 -0500

    feat: update index

--- molecules/bnpl-combined-messaging/index.js
+++ molecules/bnpl-combined-messaging/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+
 import PropTypes from 'prop-types';
 import Typography from '../../atoms/typography';
 import calculateInstallments from '../../utils/calculateBNPLInstallments';
