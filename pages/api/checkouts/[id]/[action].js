4bdcfced5c522592392558d1fccbf42a4914ea28
DevInnovator10 <devinnovator10@gmail.com>
2025-01-13 00:00:00 -0500

    feat: update [...cats]

---
+++ pages/api/checkouts/[id]/[action].js
@@ -0,0 +1,59 @@
+import { standardError } from '../../../../utils/standardizedFetch';
+import SCOPE_TYPES from '../../../../utils/sentryScopeTypes';
+import API from '../../../../utils/api';
+import withLogs from '../../../../utils/withLogs';
+
+const handler = async (req, res) => {
+  res.setHeader('X-Cache-Control', ['true']);
+
+  const {
+    body,
+    cookies,
+    headers,
+    method,
+    query: { id, action }
+  } = req;
+
+  const api = new API({
+    fingerprint: __filename.replace('pages', '').split('.')[0],
+    base: process.env.SOLIDUS_HOST,
+    host: SCOPE_TYPES.SERVICES.SOLIDUS,
+    request: req,
+    scopes: [
+      SCOPE_TYPES.API,
+      SCOPE_TYPES.SERVICES.SOLIDUS
+    ],
+    headers,
+    ...cookies
+  });
+
+  let response;
+
+  switch (method) {
+    case 'PUT':
+      response = await api.setPath(`/api/checkouts/${id}/${action}`).put(body);
+      res.status(response?.status ?? 500).json(response?.data ?? null);
+      break;
+
+    default:
+      res.setHeader('Allow', ['PUT']);
+      res.status(405).json({
+        status: 405,
+        statusText: 'Method Not Allowed',
+        data: { errors: [standardError({ status: 405, error: `Method ${method} Not Allowed` })] }
+      });
+  }
+
+  return {
+    response,
+    method,
+    body: req?.body ?? null,
+    query: req?.query ?? null,
+    status: api?.response?.status ?? 500,
+    headers,
+    uri: `/api/checkouts/${id}/${action}`,
+    filename: __filename
+  };
+};
+
+export default withLogs(handler);
