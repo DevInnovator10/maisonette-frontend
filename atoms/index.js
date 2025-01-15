666c93b8ce5733094747cf1b0065c08b64d47b13
DevInnovator10 <devinnovator10@gmail.com>
2025-01-15 00:00:00 -0500

    chore: update test.js

---
+++ atoms/index.js
@@ -0,0 +1,88 @@
+import Anchor from './anchor';
+import Button from './button';
+import ButtonChat from './button-chat';
+import Checkbox from './checkbox';
+import IconArrow from './icon-arrow';
+import IconQuestionCircle from './icon-question-circle';
+import IconCart from './icon-cart';
+import IconCircleArrow from './icon-circle-arrow';
+import IconCirclePlus from './icon-circle-plus';
+import IconCross from './icon-cross';
+import IconGift from './icon-gift';
+import IconGlobe from './icon-globe';
+import IconHeart from './icon-heart';
+import IconIceCream from './icon-ice-cream';
+import IconLabelBackInStock from './icon-label-backinstock';
+import IconLabelExclusive from './icon-label-exclusive';
+import IconLabelJusinIn from './icon-label-justin';
+import IconLabelLowStock from './icon-label-lowstock';
+import IconLabelMostWished from './icon-label-mostwished';
+import IconLabelOnSale from './icon-label-onsale';
+import IconLabelPopular from './icon-label-popular';
+import IconLabelSellingFast from './icon-label-sellingfast';
+import IconLockClosed from './icon-lock-closed';
+import IconLockOpened from './icon-lock-opened';
+import IconPaypal from './icon-paypal';
+import IconSearch from './icon-search';
+import IconClose from './icon-close';
+import IconShirt from './icon-tshirt';
+import IconSocialEmail from './icon-social-email';
+import IconSocialFacebook from './icon-social-facebook';
+import IconSocialInstagram from './icon-social-instagram';
+import IconSocialPinterest from './icon-social-pinterest';
+import IconSocialTwitter from './icon-social-twitter';
+import InputEmail from './input-email';
+import InputRange from './input-range';
+import InputSearch from './input-search';
+import InputText from './input-text';
+import Picture from './picture';
+import Radio from './radio';
+import Select from './select';
+import Video from './video';
+import Typography from './typography';
+
+export {
+  Anchor,
+  Button,
+  ButtonChat,
+  Checkbox,
+  IconArrow,
+
+  IconQuestionCircle,
+  IconCart,
+  IconCircleArrow,
+  IconCirclePlus,
+  IconCross,
+  IconGift,
+  IconGlobe,
+  IconHeart,
+  IconIceCream,
+  IconLabelBackInStock,
+  IconLabelExclusive,
+  IconLabelJusinIn,
+  IconLabelLowStock,
+  IconLabelMostWished,
+  IconLabelOnSale,
+  IconLabelPopular,
+  IconLabelSellingFast,
+  IconLockClosed,
+  IconLockOpened,
+  IconPaypal,
+  IconSearch,
+  IconShirt,
+  IconSocialEmail,
+  IconSocialFacebook,
+  IconSocialInstagram,
+  IconSocialPinterest,
+  IconSocialTwitter,
+  IconClose,
+  InputEmail,
+  InputRange,
+  InputSearch,
+  InputText,
+  Picture,
+  Radio,
+  Select,
+  Video,
+  Typography
+};
