50211c7e4af2d54df27ec90a026bdb44e164fd02
DevInnovator10 <devinnovator10@gmail.com>
2025-02-23 00:00:00 -0500

    feat: update index

---
+++ utils/tests/navigation.js
@@ -0,0 +1,9 @@
+import response from './navigation.json';
+import incorrectRes from './incorrectRes.json';
+import incorrectRes2 from './incorrectRes2.json';
+
+export {
+  response,
+  incorrectRes,
+  incorrectRes2
+};
