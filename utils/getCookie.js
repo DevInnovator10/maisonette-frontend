82cc01824638b0c8fbf191a606c00153fd7051fe
DevInnovator10 <devinnovator10@gmail.com>
2025-07-29 00:00:00 -0400

    chore: update index

---
+++ utils/getCookie.js
@@ -0,0 +1,13 @@
+const getCookie = (name, req) => {
+  const cookie = {};
+
+  const cookieLocation = req?.headers?.cookie ?? global.document.cookie;
+  cookieLocation.split(';').forEach((x) => {
+    const [k, v] = x.split('=');
+    cookie[k.trim()] = v;
+  });
+
+  return name ? cookie[name] : cookie;
+};
+
+export default getCookie;
