f4ab16c075fd472b5e5abd1dd9ca78e45a5fa2c3
DevInnovator10 <devinnovator10@gmail.com>
2025-09-05 00:00:00 -0400

    chore: update index

--- pages/api/minis/index.js
+++ pages/api/minis/index.js
@@ -4,7 +4,7 @@ import API from '../../../utils/api';
 import withLogs from '../../../utils/withLogs';
 
 const handler = async (req, res) => {
-  res.setHeader('X-Cache-Control', ['true']);
+    res.setHeader('X-Cache-Control', ['true']);
 
   const {
     body,
